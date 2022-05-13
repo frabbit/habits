f70d3569a42042df7bf5f8243c37a33cf011982c
frabbit <frabbit77@googlemail.com>
2022-05-13 10:06:56 +0200

    generate valid utf8 sequences instead of arbitrary texts

--- src/Habits/Domain/Email.hs
+++ src/Habits/Domain/Email.hs
@@ -3,9 +3,10 @@ module Habits.Domain.Email where
 import Data.Text (Text)
 import Test.QuickCheck (Arbitrary, arbitrary)
 import Test.QuickCheck.Instances ()
+import Test.QuickCheck.Utf8 (genValidUtf8)
 
 newtype Email = Email {unEmail :: Text} deriving (Show, Eq, Ord)
 
 instance Arbitrary Email where
   arbitrary = do
-    Email <$> arbitrary
+    Email <$> genValidUtf8
