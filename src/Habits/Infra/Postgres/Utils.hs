a05c87eb0f6a390c7ac1a2a422130783c2d34338
frabbit <frabbit77@googlemail.com>
2022-05-17 20:52:05 +0200

    cleanup imports in Habits.Infra.Postgres.Utils

--- src/Habits/Infra/Postgres/Utils.hs
+++ src/Habits/Infra/Postgres/Utils.hs
@@ -2,39 +2,12 @@
 
 module Habits.Infra.Postgres.Utils where
 
-import Control.Lens (view)
 import Control.Monad.IO.Class
   ( MonadIO,
     liftIO,
   )
 import qualified Data.Pool as P'
-import qualified Data.UUID as UUID
-import Data.UUID.V4 (nextRandom)
 import qualified Database.Persist.Postgresql as P
-import Habits.Domain.Account (Account)
-import qualified Habits.Domain.Account as A
-import Habits.Domain.AccountId (AccountId (AccountId))
-import qualified Habits.Domain.AccountNew as AN
-import Habits.Domain.AccountRepo
-  ( AccountNotFoundError
-      ( AccountNotFoundError
-      ),
-    AccountRepo
-      ( AccountRepo,
-        _add,
-        _getById,
-        _getByEmail
-      ),
-    Add,
-    GetById, GetByEmail,
-  )
-import Habits.Domain.Email (Email (..))
-import Habits.Domain.Password (Password (..))
-import qualified Habits.Infra.Postgres.Schema as S
-import Haskus.Utils.Variant.Excepts (throwE)
-import Database.Persist ((==.), SelectOpt (LimitTo))
-import qualified Veins.Data.ComposableEnv as CE
-import Data.Function ((&))
 import Control.Monad.Reader (ReaderT)
 import Control.Monad.Logger (NoLoggingT)
 import UnliftIO.Resource (ResourceT)
