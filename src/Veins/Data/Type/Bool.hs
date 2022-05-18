d2371b76e1662193dbb4d598f3cac571dbb95436
frabbit <frabbit77@googlemail.com>
2022-05-18 07:59:49 +0200

    remove List function from Bool

--- src/Veins/Data/Type/Bool.hs
+++ src/Veins/Data/Type/Bool.hs
@@ -24,11 +24,6 @@ type family Or (x :: Bool) (y :: Bool) :: Bool where
   Or _ 'True = 'True
   Or _ _ = 'False
 
-type family Elem (t :: k) (ts :: [k]) :: Bool where
-  Elem a '[] = 'False
-  Elem a (a ': as) = 'True
-  Elem a (b ': bs) = Elem a bs
-
 type (==) :: k -> k -> Bool
 type family a == b where
   f a == g b = f == g `And` a == b
