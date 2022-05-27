f3621150c50c03020276753a9d3a69d87faf91c8
frabbit <frabbit77@googlemail.com>
2022-05-27 16:05:48 +0200

    simplify register test case by extracting setup functionality

--- test-e2e/RegisterE2ESpec.hs
+++ test-e2e/RegisterE2ESpec.hs
@@ -1,36 +1,42 @@
 module RegisterE2ESpec (spec) where
 
-import Test.Hspec (Spec, describe, it)
-import qualified Network.Wai.Handler.Warp as Warp
-import           Network.HTTP.Client (defaultManagerSettings, newManager)
-import Habits.Web.Server (app, ServerConfig (ServerConfig))
-import Habits.Web.Routes.RegisterRoute (RegisterApi, RegisterResponseDto (RegisterResponseDto))
-import Data.Proxy (Proxy(Proxy))
-import Servant.Client (client, baseUrlPort, parseBaseUrl, mkClientEnv, runClientM)
-import Veins.Test.HSpec.TH (ShouldMatchPattern(shouldMatchPattern))
-import Veins.Test.QuickCheck (sampleIO)
+import Data.Proxy (Proxy (Proxy))
 import Debug.Trace (traceShowM)
 import Habits.Domain.AccountId (parseAccountId)
+import Habits.Web.Routes.RegisterRoute (RegisterApi, RegisterRequestDto, RegisterResponseDto (RegisterResponseDto))
+import Habits.Web.Server (ServerConfig (ServerConfig), app)
+import Network.HTTP.Client (defaultManagerSettings, newManager)
+import qualified Network.Wai.Handler.Warp as Warp
+import Servant.Client (ClientEnv, ClientError, ClientM, baseUrlPort, client, mkClientEnv, parseBaseUrl, runClientM)
+import Test.Hspec (Spec, describe, it)
+import Veins.Test.HSpec.TH (ShouldMatchPattern (shouldMatchPattern))
+import Veins.Test.QuickCheck (sampleIO)
 
 testConfig :: ServerConfig
 testConfig = ServerConfig
-
 
 -- testWithApplication makes sure the action is executed after the server has
 -- started and is being properly shutdown.
 withApp :: (Warp.Port -> IO ()) -> IO ()
 withApp = Warp.testWithApplication (pure $ app testConfig)
 
+getClientTestEnv :: Int -> IO ClientEnv
+getClientTestEnv port = do
+  baseUrl <- parseBaseUrl "http://localhost"
+  manager <- newManager defaultManagerSettings
+  pure $ mkClientEnv manager (baseUrl {baseUrlPort = port})
+
+register :: RegisterRequestDto -> ClientM RegisterResponseDto
+register = client (Proxy :: Proxy RegisterApi)
+
+runRegister :: Int -> RegisterRequestDto -> IO (Either ClientError RegisterResponseDto)
+runRegister port req = do
+  env <- getClientTestEnv port
+  runClientM (register req) env
+
 spec :: Spec
 spec = describe "RegisterE2E" $ do
-  it "should provide a register route" $ do
-    withApp $ \port -> do
-      baseUrl <- parseBaseUrl "http://localhost"
-      manager <- newManager defaultManagerSettings
-      let clientEnv p = mkClientEnv manager (baseUrl { baseUrlPort = p })
-      let register = client (Proxy :: Proxy RegisterApi)
-      x <- sampleIO
-      result <- runClientM (register x) (clientEnv port)
-      traceShowM result
-      $('result `shouldMatchPattern` [p|Right (RegisterResponseDto (parseAccountId -> Just _))|])
-
+  it "should provide a register route" . withApp $ \port -> do
+    req <- sampleIO
+    result <- runRegister port req
+    $('result `shouldMatchPattern` [p|Right (RegisterResponseDto (parseAccountId -> Just _))|])
