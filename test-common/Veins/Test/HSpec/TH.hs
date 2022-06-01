a6d26ad49673f1418404fcdfb8392ba64c521ae5
frabbit <frabbit77@googlemail.com>
2022-06-01 13:16:07 +0200

    show proper error message in shouldMatchPattern

--- test-common/Veins/Test/HSpec/TH.hs
+++ test-common/Veins/Test/HSpec/TH.hs
@@ -1,24 +1,24 @@
 module Veins.Test.HSpec.TH where
-import Language.Haskell.TH (Exp (VarE), Pat, Q, Name)
 
+import Language.Haskell.TH (Exp (LitE, VarE), Lit (StringL), Name, Pat, Q, pprint)
 import Test.Hspec.Expectations.Lifted (expectationFailure)
 
 class ShouldMatchPattern a where
   shouldMatchPattern :: a -> Q Pat -> Q Exp
 
+patToExp :: Pat -> Exp
+patToExp = LitE . StringL . pprint
+
 instance ShouldMatchPattern (Q Exp) where
   shouldMatchPattern :: Q Exp -> Q Pat -> Q Exp
-  shouldMatchPattern e pat = [|
-    case $e of
-      $pat -> pure ();
-      _ -> expectationFailure "Pattern does not match"
-    |]
+  shouldMatchPattern e pat = do
+    let p = patToExp <$> pat
+    [|
+      case $e of
+        $pat -> pure ()
+        v -> expectationFailure ("Expected pattern: " <> $p <> "\nActual value: " <> show v)
+      |]
+
 instance ShouldMatchPattern Name where
   shouldMatchPattern :: Name -> Q Pat -> Q Exp
-  shouldMatchPattern n pat = [|
-    case $e of
-      $pat -> pure ();
-      _ -> expectationFailure "Pattern does not match"
-    |]
-    where
-      e = pure $ VarE n
+  shouldMatchPattern n = shouldMatchPattern (pure $ VarE n :: Q Exp)
